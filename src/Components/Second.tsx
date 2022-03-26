3cc0c9ee172d7eacb4a2d277762d9fd706c2d5ea
Rakshit78 <rakshitsootwal@yahoo.com>
2022-03-27 00:33:52 +0530

    change

--- src/Components/Second.tsx
+++ src/Components/Second.tsx
@@ -1,47 +1,83 @@
-import { useState, useEffect } from 'react'
-import Third from './Third'
+import { useState, useEffect } from 'react';
+import Third from './Third';
 import Button from '@mui/material/Button';
-import CircularProgress from '@mui/material/CircularProgress'
+import CircularProgress from '@mui/material/CircularProgress';
 type Props = {
-  input: string,
-  navigate:any
-}
+  input: string;
+  navigate: any;
+};
 export default function Second(props: Props) {
   const [list, setlist] = useState([]);
-  const [capital,setcapital]=useState(false);
-  const [loading,setloading]=useState(true);
-  const [err,seterr]=useState(false);
-  async function getdata(){
-    try{
-   const res=await fetch(`https://restcountries.com/v3.1/name/${props.input}`);
-   const data=await res.json();
-   setlist(data);
-   setloading(false);
-    }catch(e){
-     console.log(e)
+  const [capital, setcapital] = useState(false);
+  const [loading, setloading] = useState(true);
+  const [err, seterr] = useState(false);
+  const [query, setquery] = useState('');
+  async function getdata() {
+    try {
+      const res = await fetch(
+        `https://restcountries.com/v3.1/name/${props.input}`
+      );
+      const data = await res.json();
+      setlist(data);
+      setquery(data[0].capital);
+      console.log(query);
+      setloading(false);
+    } catch (e) {
+      console.log(e);
     }
- }
+  }
+
   useEffect(() => {
-   getdata();
- 
-  }, [])
+    getdata();
+  }, []);
   return (
-    <div style={{"width":"100vw", "height":"100vh","display":"grid","placeItems":"center"}}>
-      {loading?<CircularProgress/>:<div style={{"background":"#e5e5e5","width":"400px","height":"450px","padding":"10px"}}>
-      <Button variant="contained" style={{  }} onClick={()=>props.navigate("/")}>Back</Button>
-      {list.map(({ capital, population, latlng, flag, flags:{png} }) => {
-        return <div key={capital}>
-          <p>Capital: {capital}</p>
-          <p>Population: {population}</p>
-          <p>Lating: {latlng}</p>
-          <p>Country: {flag}</p>
-          <img src={png} alt="image" width="30px" height="30px" />
-         
+    <div
+      style={{
+        width: '100vw',
+        height: '100vh',
+        display: 'grid',
+        placeItems: 'center',
+      }}
+    >
+      {loading ? (
+        <CircularProgress />
+      ) : (
+        <div
+          style={{
+            background: '#e5e5e5',
+            width: '400px',
+            height: '450px',
+            padding: '10px',
+          }}
+        >
+          <Button
+            variant='contained'
+            style={{}}
+            onClick={() => props.navigate('/')}
+          >
+            Back
+          </Button>
+          {list.map(({ capital, population, latlng, flag, flags: { png } }) => {
+            return (
+              <div key={capital}>
+                <p>Capital: {capital}</p>
+                <p>Population: {population}</p>
+                <p>Lating: {latlng}</p>
+                <p>Country: {flag}</p>
+                <img src={png} alt='image' width='30px' height='30px' />
+              </div>
+            );
+          })}
+          <Button
+            variant='contained'
+            style={{ margin: '10px', marginBottom: '100px' }}
+            onClick={() => setcapital(!capital)}
+          >
+            Capital wheather
+          </Button>
+          {capital && <Third query={query} />}
         </div>
-      })}
-      <Button variant="contained" style={{ "margin": "10px", "marginBottom": "100px" }} onClick={()=>setcapital(!capital)}>Capital wheather</Button>
-      {capital && <Third/>}
-     </div>}
+      )}
     </div>
-  )
+  );
 }
--- src/Components/Third.tsx
+++ src/Components/Third.tsx
@@ -1,29 +1,45 @@
-import { useEffect,useState } from "react";
-export default function Third() {
-  const [list,setlist]=useState([]);
-  let arr:any=[];
-  async function getdata(){
-    const res=await fetch(`http://api.weatherstack.com/current?access_key=f97561106c9153d29b4081f510940097&query=Delhi`);
-    const data= await res.json();
-    arr=[data];
+import { useEffect, useState } from 'react';
+type Props = {
+  query: any;
+};
+export default function Third(props: Props) {
+  const [list, setlist] = useState([]);
+  let arr: any = [];
+  async function getdata() {
+    const res = await fetch(
+      `http://api.weatherstack.com/current?access_key=d4ef5175c0d0339c0eea6c2af742d5ec&query=${props.query}`
+    );
+    const data = await res.json();
+    arr = [data];
     setlist(arr);
-    console.log(arr);
+    //console.log(arr);
   }
-  useEffect(()=>{
-    getdata()
-  
-  },[])
-  return <div style={{"marginTop":"-100px"}}>
- 
-   {list.map(({current:{temperature,weather_icons,wind_speed,Trecit}})=>{
-  return<div key={temperature}>
-<p>Temp : {temperature}</p>
-<p>Whether_icon : <img src={weather_icons} alt="logo" width="20px" height="20px"/></p>
-<p>Wind_speed : {wind_speed}</p>
-<p>Trecit: {Trecit}</p>
-  </div>
-})}
- 
-  </div>;
+  useEffect(() => {
+    getdata();
+    console.log(props.query);
+  }, []);
+  return (
+    <div style={{ marginTop: '-100px' }}>
+      {list.map(
+        ({ current: { temperature, weather_icons, wind_speed, Trecit } }) => {
+          return (
+            <div key={temperature}>
+              <p>Temp : {temperature}</p>
+              <p>
+                Whether_icon :{' '}
+                <img
+                  src={weather_icons}
+                  alt='logo'
+                  width='20px'
+                  height='20px'
+                />
+              </p>
+              <p>Wind_speed : {wind_speed}</p>
+              <p>Trecit: {Trecit}</p>
+            </div>
+          );
+        }
+      )}
+    </div>
+  );
 }
-
